720f53ec18ad08439eded81a824e41de045ab435
Maya Lu <lumaya630@gmail.com>
2024-07-05 18:02:49 +0800

    adding pset5

---
+++ psets/pset5/student/tests/q1a.R
@@ -0,0 +1,20 @@
+test = list(
+  name = "q1a",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 1.0,
+      code = {
+        answers <- c(i, ii, iii, iv)
+        question.correct_b <- all(str_length(answers) == 1)
+        question.correct_a <- all(str_detect(answers,"[[:upper:]]"))
+
+        testthat::expect_true(question.correct_b,
+             info = "Must be in upper-case")
+        testthat::expect_true(question.correct_a,
+             info = "Please format as a single letter")
+      }
+    )
+  )
+)

8117ad4de521badbbc024588d90038a35b4ad1e5
Maya Lu <lumaya630@gmail.com>
2024-07-06 12:57:13 +0800

    updates pset5

--- psets/pset5/student/tests/q2f.R
+++ psets/pset5/student/tests/q2f.R
@@ -1,19 +1,19 @@
 test = list(
-  name = "q1b",
+  name = "q2f",
   cases = list(
     ottr::TestCase$new(
       hidden = FALSE,
       name = NA,
-      points = 1.0,
+      points = 1.5,
       code = {
-        answers <- c(i, ii, iii, iv, v)
+        answers <- c(d)
         question.correct_b <- all(str_length(answers) == 1)
         question.correct_a <- all(str_detect(answers,"[[:upper:]]"))
 
         testthat::expect_true(question.correct_b,
              info = "Must be in upper-case")
         testthat::expect_true(question.correct_a,
-             info = "Please format as a single letter \"T\" or \"F\"")
+             info = "Please format as a single letter")
       }
     )
   )

0de9d8e583d912b0a1bcf8effcc4e958ba2166dd
Maya Lu <lumaya630@gmail.com>
2024-07-17 20:24:30 +0800

    pset 1 updates

--- psets/pset1/student/tests/q3a.R
+++ psets/pset1/student/tests/q3a.R
@@ -9,7 +9,7 @@ test = list(
         sol_n_cases <- nrow(ctdc)
         question.correct <- all.equal(n_cases, sol_n_cases)
         testthat::expect_true(question.correct,
-             info = "Not quite right. Try again!")
+             info = "Did you use the nrow() function on the ctdc dataset?")
       }
     )
   )

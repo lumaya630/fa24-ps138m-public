1335246bd6ed9c3b8a4f2c7b20b0e8746c85b51a
Maya Lu <lumaya630@gmail.com>
2024-08-20 12:41:25 -0700

    discussion1

--- psets/pset1/student/tests/q4d.R
+++ psets/pset1/student/tests/q4d.R
@@ -4,15 +4,26 @@ test = list(
     ottr::TestCase$new(
       hidden = FALSE,
       name = NA,
-      points = 2.0,
+      points = 1,
       code = {
         df.soln <- ctdc %>%
                 filter((isForcedLabour >0 | isSexualExploit > 0 | isOtherExploit >0)) %>% 
                 select(starts_with("recruiter"))
 
+        question.correct <- all.equal(df.soln, recruiter_ht)
+        testthat::expect_true(question.correct, 
+                             info = "Your recruiter_ht dataframe doesnt seem right.")
+      }
+    ),
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 1,
+      code = {
         summary.soln <- summary(df.soln)
         question.correct <- all.equal(summary.soln, recruiter_summary)
-        testthat::expect_true(question.correct)
+        testthat::expect_true(question.correct,
+                             info = "Your summary doesn't seem right.")
       }
     )
   )

3db9b4b01715e7e941f86fb6255fed6a235d668a
Maya Lu <lumaya630@gmail.com>
2024-09-24 15:10:13 -0700

    pset2

--- psets/pset2/student/tests/q2b.R
+++ psets/pset2/student/tests/q2b.R
@@ -6,7 +6,7 @@ test = list(
       name = NA,
       points = 0,
       code = {
-        answers <- c(q2b_i, q2b_ii,q2b_iii)
+        answers <- c(q2b_i, q2b_ii)
         question.correct_b <- all(str_length(answers) == 1)
         question.correct_a <- all(str_detect(answers,"[[:upper:]]"))
         question.correct_c <- all(str_detect(answers,"A|B"))

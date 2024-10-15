20e909e4630763f6e6dd81fa1edc7ef256e0f123
Maya Lu <lumaya630@gmail.com>
2024-10-15 00:33:51 -0700

    section6

--- discussion/section6/tests/Q4.R
+++ discussion/section6/tests/Q4.R
@@ -7,7 +7,7 @@ test = list(
       points = 0.0,
       code = {
         # YOUR CODE HERE
-        no_fom.sol <- my_dat %>% filter(ud_freedom_movement == "Never") # SOLUTION
+        no_fom.sol <- my_dat %>%  filter(ud_freedom_movement %in% c("Sometimes", "Often", "Refused to answer", "Yes, all of the time")) # SOLUTION
         testthat::expect_true(all.equal(no_fom.sol, no_fom))
       }
     )
--- discussion/section6/tests/Q8.R
+++ discussion/section6/tests/Q8.R
@@ -7,10 +7,10 @@ test = list(
       points = 0.0,
       code = {
         # YOUR CODE HERE
-        avg_knowledge_by_nat.sol <- no_fom %>% group_by(nationality) %>%
-                        summarise(avg_knowledge_score = mean(score, na.rm = T))
+        mod.sol <- lm(score~treated, data = no_fom) # SOLUTION
 
-        testthat::expect_true(all.equal(avg_knowledge_by_nat.sol, avg_knowledge_by_nat))
+
+        testthat::expect_true(all.equal(mod.sol, mod))
       }
     )
   )

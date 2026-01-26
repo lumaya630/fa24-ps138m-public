e9bc0b9770b3f9e8674f9b7f2fdde43114c2df2d
Shreya Pabbaraju <shreyshrey@wifi-10-43-124-6.wifi.berkeley.edu>
2026-01-26 15:22:12 -0800

    Edits to pset 1

--- psets/pset1/student/tests/q4a.R
+++ psets/pset1/student/tests/q4a.R
@@ -1,16 +1,5 @@
 test = list(
   name = "q4a",
   cases = list(
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 1.0,
-      code = {
-        df.soln <- ctdc %>% filter(isForcedLabour == 1 | isSexualExploit == 1 | isOtherExploit==1) 
-
-        question.correct <- all.equal(df.soln, victims.df )
-        testthat::expect_true(question.correct)
-      }
-    )
   )
 )
--- psets/pset1/student/tests/q4b.R
+++ psets/pset1/student/tests/q4b.R
@@ -1,16 +1,5 @@
 test = list(
   name = "q4b",
   cases = list(
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 2.0,
-      code = {
-        df.soln <- victims.df %>% select(meansThreats, meansDebtBondageEarnings, meansAbusePsyPhySex) 
-
-        question.correct <- all.equal(df.soln, means.df )
-        testthat::expect_true(question.correct)
-      }
-    )
   )
 )
